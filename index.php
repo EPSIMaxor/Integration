d35dded63ebe29c9750c4cc7f16d1e0b4a76276a
morainppe <gobole20@gmail.com>
2017-12-15 14:33:33 +0100

    First version of restapi

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=inte', 'dev', 'password');
+	}
+	catch( PDOException $Exception ) {
+	    	echo 'Erreur de connexion : ' . $Exception->getMessage() . ' ; code : ' . $Exception->getCode();
+	}
+
+	$method = $_SERVER['REQUEST_METHOD'];
+	$path = $_SERVER['REQUEST_URI'];
+	
+	$output = array();
+
+	$output['info'] = array(
+		'method' => $method,
+		'path' => $path
+	);
+
+
+	if($path == '/rest/users/') {
+		
+		if($method == 'GET') {
+
+			$output['users'] = array();
+
+			$req = $bdd->prepare('SELECT * FROM users;');
+			$req->execute();
+			while($row = $req->fetch()) {
+				$output['users'][] = array(
+					'username' => $row['username'],
+					'token' => $row['token'],
+					'link' => '/rest/users/' . $row['id']
+				);
+			}
+		}
+		else {
+
+			header("HTTP/1.1 400 Bad Request");
+			$output['error'] = array(
+				'code' => 5,
+				'message' => 'Invalid method for this path'
+			);
+		}
+
+	}
+	elseif(strpos($path, '/rest/users/') === 0) {
+		$id = substr($path, strlen('/rest/users/'));
+		
+		if($method == 'GET') {
+
+
+			$output['users'] = array();
+
+			$req = $bdd->prepare('SELECT * FROM users where id = :id;');
+			$req->bindParam(':id', $id, PDO::PARAM_INT);
+			$req->execute();
+			while($row = $req->fetch()) {
+				$output['users'][] = array(
+					'username' => $row['username'],
+					'token' => $row['token'],
+					'link' => '/rest/users/' . $row['id']
+				);
+			}
+		}
+		else {
+
+			header("HTTP/1.1 400 Bad Request");
+			$output['error'] = array(
+				'code' => 5,
+				'message' => 'Invalid method for this path'
+			);
+		}
+
+	}
+
+
+	header('Content-Type: application/json');
+	echo json_encode($output);
+
+?>
